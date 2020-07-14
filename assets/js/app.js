8e6284b866fbd1f7e748a13a68ed99eac16f80de
Christian Deacon <gamemann@gflclan.com>
2020-07-14 03:12:03 +0000

    Added Bootstrap toggle.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,9 @@
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
+import 'jquery'
 import 'bootstrap'
+import 'bootstrap4-toggle'
 import "../css/app.scss"
 
 // webpack automatically bundles all modules in your
